30430f3eafaf0b6b5021162c20d77504135fde9f
Shan Siddiqui <shan.d.siddiqui@gmail.com>
2022-05-26 00:35:35 -0400

    First and last commit

---
+++ db/migrate/20220525035616_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :location
+      t.string :team
+      t.string :job_title
+      t.string :url
+      t.timestamps
+    end
+  end
+end
